9d9a73295bb8c96a758b2be258e7fc75b6841e03
edzen12 <oichiev.edzen@gmail.com>
2023-03-02 13:07:12 +0600

    edit

--- studoi/settings.py
+++ studoi/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATIC_ROOT = BASE_DIR / 'static' # для collectstatic вкл, а если нет, то выкл
 STATIC_DIR = BASE_DIR / 'static'
 STATICFILES_DIRS = [STATIC_DIR]
 
